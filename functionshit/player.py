8d16f87638e9ed271d926e74935c2875eac0e713
algot <algot.lindblom@gmail.com>
2022-11-30 09:46:07 +0100

    la till skit i main få se vad som händer

--- functionshit/player.py
+++ functionshit/player.py
@@ -1,12 +1,15 @@
 class player:
+    inventory = []
     def __init__(self):
         self.name = input('Vad heter du? \n')
     
     def vadheterjag(self):
-        self.vadheterjag.local = 'local'
         print(f'Du heter {self.name}')
+
+    def newItem(self, item):
+        self.inventory.append(item)
 
 p = player()
 p.vadheterjag()
 l = p.vadheterjag().local
-    
+print(p.inventory)
