ffdb0e1bc1f9088a75d5e028c9980f957a9fb470
Goalkee32 <67052974+Goalkee32@users.noreply.github.com>
2022-12-13 21:06:13 +0100

    Började på våning 2

---
+++ functionshit/floor2.py
@@ -0,0 +1,19 @@
+import time
+
+class Tower:
+    def coolText(self, text, speed, nl):
+        for i in text:
+            print(i, end='')
+            time.sleep(speed)
+        if nl:
+            print('')
+
+    def floor2(self):
+        self.coolText('Du går upp för den mörka trappan. Dina steg ekar i det tomma tornet.', 0.02, True)
+        self.coolText('Framför dig ser du en korridor med två vägar', 0.02, True)
+        if input('Vill du gå åt höger eller vänster? ').lower() != "Höger":
+            self.coolText('Du rycker i dörren men den är låst. Du testar dörren åt vänster istället. ', 0.05, True)
+        self.coolText('Handtaget är löst. Du öppnar dörren och ett rum lyses upp av din fackla.', 0.02, True)
+
+T = Tower()
+T.floor2()
